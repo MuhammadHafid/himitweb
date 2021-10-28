5f57d05bb75b956e5b2c8b1c0e309b2e7e862850
Muhammad Hafid <hafidazis385@gmail.com>
2021-10-28 08:07:09 +0700

    landing add section 6,7

---
+++ owlcarousel.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        items: 3,
+        autoplay: true,
+        center: true,
+        // nav: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        responsiveClass: true,
+    });
+})
